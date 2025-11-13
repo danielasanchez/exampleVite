fd5233de3a1673887964f059f0791d53e79abc9a
Daniela Sánchez <danielasanchez.itt@hotmail.com>
2025-11-13 10:09:59 -0800

    props, funciones y componentes

--- src/Componentes/Formulario.jsx
+++ src/Componentes/Formulario.jsx
@@ -4,65 +4,63 @@ import {Button, Form} from 'react-bootstrap';
 function Formulario(props){
   const {handleSubmit,saveChange,alumno,desh}=props;
     return(
-<div className="Form">
-
-            <Form onSubmit={handleSubmit}>
-              <Form.Group className="mb-3">
-                <Form.Label>Matrícula</Form.Label>
-                <Form.Control 
-                   placeholder="Ingresa tu matrícula"
-                   onChange={saveChange}
-                   value={alumno.matricula}
-                   name="matricula"
-                   disabled={desh}
-                
+      <div className="Form">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3">
+            <Form.Label>Matrícula</Form.Label>
+            <Form.Control 
+                placeholder="Ingresa tu matrícula"
+                onChange={saveChange}
+                value={alumno.matricula}
+                name="matricula"
+                disabled={desh}
+            
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control 
+                placeholder="Ingresa tu nombre completo"
+                onChange={saveChange}
+                value={alumno.nombre}
+                name="nombre"
+            />
+            
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Correo</Form.Label>
+            <Form.Control type="email" 
+                placeholder="Ingresa tu correo"
+                onChange={saveChange}
+                value={alumno.correo}
+                name="correo"
                 />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Nombre</Form.Label>
-                <Form.Control 
-                   placeholder="Ingresa tu nombre completo"
-                   onChange={saveChange}
-                   value={alumno.nombre}
-                   name="nombre"
-                />
-                
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Correo</Form.Label>
-                <Form.Control type="email" 
-                   placeholder="Ingresa tu correo"
-                   onChange={saveChange}
-                   value={alumno.correo}
-                   name="correo"
-                   />
-                <Form.Text className="text-muted">
-                  Nunca compartiremos su correo electrónico con nadie más.
-                </Form.Text>
-              </Form.Group>
+            <Form.Text className="text-muted">
+              Nunca compartiremos su correo electrónico con nadie más.
+            </Form.Text>
+          </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label>Carrera</Form.Label>
-                  <Form.Select
-                   onChange={saveChange}
-                   value={alumno.carrera}
-                   name="carrera"
-                  >
-                    <option value="selecciona">selecciona</option>
-                    <option value="Informatica">Informática</option>
-                    <option value="Sistemas">Sistemas Computacionales</option>
-                    <option value="TICS">TICS</option>
-                    <option value="IA">IA </option>
-                    <option value="Ciberseguridad">Ciberseguridad</option>
-                  </Form.Select>
-              </Form.Group>
-            <Button variant="primary" type="submit">
-              Enviar
-            </Button>
+          <Form.Group className="mb-3">
+            <Form.Label>Carrera</Form.Label>
+              <Form.Select
+                onChange={saveChange}
+                value={alumno.carrera}
+                name="carrera"
+              >
+                <option value="selecciona">selecciona</option>
+                <option value="Informatica">Informática</option>
+                <option value="Sistemas">Sistemas Computacionales</option>
+                <option value="TICS">TICS</option>
+                <option value="IA">IA </option>
+                <option value="Ciberseguridad">Ciberseguridad</option>
+              </Form.Select>
+          </Form.Group>
+        <Button variant="primary" type="submit">
+          Enviar
+        </Button>
 
-            </Form>
-          </div>
-
+        </Form>
+      </div>
     );
 }
 
--- src/Componentes/Listado.jsx
+++ src/Componentes/Listado.jsx
@@ -3,41 +3,35 @@ import {Button, Table} from 'react-bootstrap';
 function Listado(props){
     const {lista, modificar, eliminar} = props;
     return(
-<div className="List">
-            
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th># Matricula</th>
-                    <th>Nombre</th>
-                    <th>Correo</th>
-                    <th>Carrera</th>
-                    <th></th>
-                    <th></th>
+      <div className="List">
+        <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th># Matricula</th>
+                <th>Nombre</th>
+                <th>Correo</th>
+                <th>Carrera</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+
+              {
+                lista.map((a,i)=>
+                  <tr key={i}>
+                    <td>{a.matricula}</td>
+                    <td>{a.nombre}</td>
+                    <td>{a.correo}</td>
+                    <td>{a.carrera}</td>
+                    <td><Button variant="primary" onClick={()=>modificar(a)}> Modificar</Button></td>
+                    <td><Button variant="danger" onClick={()=>eliminar(a.matricula)}> Eliminar</Button></td>
                   </tr>
-                </thead>
-                <tbody>
-
-                  {
-                    lista.map((a,i)=>
-                      <tr key={i}>
-                        <td>{a.matricula}</td>
-                        <td>{a.nombre}</td>
-                        <td>{a.correo}</td>
-                        <td>{a.carrera}</td>
-                        <td><Button variant="primary" onClick={()=>modificar(a)}> Modificar</Button></td>
-                        <td><Button variant="danger" onClick={()=>eliminar(a.matricula)}> Eliminar</Button></td>
-                      </tr>
-                    )
-                  }
-
-                </tbody>
-              </Table>
-
-          </div>
-
-
-
+                )
+              }
+            </tbody>
+        </Table>
+      </div>
     );
 }
 
